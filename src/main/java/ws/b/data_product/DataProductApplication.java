812937d3e2d60bec327ccd97098054976a90c907
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2022-12-23 09:18:54 +0700

    menambah index.html

---
+++ src/main/java/ws/b/data_product/DataProductApplication.java
@@ -0,0 +1,13 @@
+package ws.b.data_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/b/data_product/DataProductApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.b.data_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
